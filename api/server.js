019f7273dbc476385781904f064764a86aff4de3
Anthony Coman <acoman@yahoo.com>
2022-02-02 13:17:23 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -2,15 +2,14 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 const recipesRouter = require('./recipe/recipes-router')
-
-const authRouter = require('./auth/auth-router')
+// const authRouter = require('./auth/auth-router')
 
 const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 server.use('/api/recipes', recipesRouter)
-server.use('/api/auth', authRouter)
+// server.use('/api/auth', authRouter)
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
