e247a2e849ce6734ba793b26d8476cea91fed0f9
Anthony Coman <acoman@yahoo.com>
2022-02-03 15:24:51 -0600

    blehh

--- api/data/migrations/20220131192632_second-migration.js
+++ api/data/migrations/20220131192632_second-migration.js
@@ -24,10 +24,6 @@ exports.up = async (knex) => {
 }
 
 exports.down = async (knex) => {
-	await knex.schema.dropTableIfExists('recipes_categories')
-	await knex.schema.dropTableIfExists('categoreies')
-  await knex.schema.dropTableIfExists('ingredients_steps')
   await knex.schema.dropTableIfExists('steps')
-  await knex.schema.dropTableIfExists('ingredients')
   await knex.schema.dropTableIfExists('recipes')
 }
