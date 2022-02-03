a760a1f8b2e3abf90076df89d6b6c13e77a953ed
Anthony Coman <acoman@yahoo.com>
2022-02-03 15:15:43 -0600

    Big Trouble In Little China

--- api/data/migrations/20220131192632_second-migration.js
+++ api/data/migrations/20220131192632_second-migration.js
@@ -24,6 +24,8 @@ exports.up = async (knex) => {
 }
 
 exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('ingredients_steps')
   await knex.schema.dropTableIfExists('steps')
+  await knex.schema.dropTableIfExists('ingredients')
   await knex.schema.dropTableIfExists('recipes')
 }
